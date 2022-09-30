110e80fa96def7acc3d0f519bf9eea7be55e08b4
KJHJason <kuanjunhaojason@gmail.com>
2022-09-30 23:36:16 +0800

    update user agent

--- src/utils/constants.go
+++ src/utils/constants.go
@@ -17,7 +17,7 @@ func GetUserAgent() string {
 	if (userAgentOS == "") {
 		userAgentOS = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 	}
-	return userAgentOS + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+	return userAgentOS + " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 }
 
 func GetAppPath() string {
